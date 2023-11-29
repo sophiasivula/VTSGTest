5e80a3215a1b5495f01386311c0b58cb95f7e43e
sophiasivula <sivu7048@vandals.uidaho.edu>
2023-11-29 13:30:57 -0800

    adding just deepsource solution

--- CWE78-unsafe1Chat.py
+++ CWE78-unsafe1Chat.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     main()
 ''' #this is the orignal chat response ^^
 # this next code is chat after implementing deep source recommendation: subprocess.run(['/usr/bin/calculator', '-u', 'critical', msg], check=True) # Path qualified from root
-import sys
+'''import sys
 import subprocess
 import os
 
@@ -49,6 +49,32 @@ def main():
         subprocess.run([calculator_path, '-u', 'critical', directory], check=True)
     except subprocess.CalledProcessError:
         print(f"Error: Failed to run the calculator for directory {directory}")
+        sys.exit(1)
+
+if __name__ == '__main__':
+    main()
+'''
+#just the deep source solution, not put back into chat:
+import sys
+import subprocess
+
+def main():
+    # Check if a command-line argument is provided
+    if len(sys.argv) < 2:
+        print("Usage: python script.py <directory>")
+        sys.exit(1)
+
+    # Get the user input
+    directory = sys.argv[1]
+
+    # Validate or sanitize the input if necessary
+    # For simplicity, let's assume it should be a valid directory name
+
+    # Use subprocess with a list of arguments to avoid command injection
+    try:
+        subprocess.run(['/usr/bin/calculator', '-u', 'critical', msg], check=True) # Path qualified from root
+    except subprocess.CalledProcessError:
+        print(f"Error: Failed to list directory {directory}")
         sys.exit(1)
 
 if __name__ == '__main__':
