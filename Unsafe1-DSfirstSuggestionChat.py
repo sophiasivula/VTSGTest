f50b0b40de5bbe8abc6a2248fe9bc56598b14ac7
sophiasivula <sivu7048@vandals.uidaho.edu>
2023-11-30 18:01:18 -0800

    Update Unsafe1-DSfirstSuggestionChat.py

--- Unsafe1-DSfirstSuggestionChat.py
+++ Unsafe1-DSfirstSuggestionChat.py
@@ -25,6 +25,7 @@ if __name__ == '__main__':
 
 '''
 
+'''
 import sys
 import subprocess
 import shlex
@@ -53,4 +54,31 @@ def main():
 if __name__ == '__main__':
     main()
 
+'''
+
+import sys
+import subprocess
+import shlex
+
+def main():
+    # Check if a command-line argument is provided
+    if len(sys.argv) < 2:
+        print("Usage: python script.py <directory>")
+        sys.exit(1)
+
+    # Get the user input
+    directory = sys.argv[1]
+
+    # Validate or sanitize the input if necessary
+    # For simplicity, let's assume it should be a valid directory name
+
+    # Use subprocess with a list of arguments to avoid command injection
+    try:
+        subprocess.run(['/usr/bin/ls', '-u', 'critical', shlex.quote(directory)], check=True)
+    except subprocess.CalledProcessError:
+        print(f"Error: Failed to list directory {directory}")
+        sys.exit(1)
+
+if __name__ == '__main__':
+    main()
 
