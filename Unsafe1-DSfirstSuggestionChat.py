85cc76e662d7dd1f3db3ff8d632d704133a89296
sophiasivula <sivu7048@vandals.uidaho.edu>
2023-11-30 17:25:22 -0800

    added DS first suggetion

---
+++ Unsafe1-DSfirstSuggestionChat.py
@@ -0,0 +1,24 @@
+import sys
+import subprocess
+
+def main():
+    # Check if a command-line argument is provided
+    if len(sys.argv) < 2:
+        print("Usage: python script.py <directory>")
+        sys.exit(1)
+
+    # Get the user input
+    directory = sys.argv[1]
+
+    # Validate or sanitize the input if necessary
+    # For simplicity, let's assume it should be a valid directory name
+
+    # Use subprocess with a list of arguments to avoid command injection
+    try:
+        subprocess.run(['ls', directory])
+    except subprocess.CalledProcessError:
+        print(f"Error: Failed to list directory {directory}")
+        sys.exit(1)
+
+if __name__ == '__main__':
+    main()
