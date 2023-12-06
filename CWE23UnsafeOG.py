253d33a733b43e1258e5438aa73999b92a5313ec
sophiasivula <sivu7048@vandals.uidaho.edu>
2023-12-05 18:03:05 -0800

    Update CWE23UnsafeOG.py

--- CWE23UnsafeOG.py
+++ CWE23UnsafeOG.py
@@ -1,34 +1,18 @@
 import math
+import os
 import sys
 
 
 def main():
+    tainted_0 = sys.argv[1]
+    tainted_1 = tainted_0
 
-    try:
-        tainted_2 = int(sys.argv[1])
-    except ValueError:
-        tainted_2 = 1776
+    # No filtering (sanitization)
+    tainted_1 = tainted_0
 
-    tainted_3 = tainted_2
 
-    if 1==0:
-
-        if tainted_2 < 0:
-            sys.exit("Negative input not allowed")
-        tainted_3 = tainted_2
-
-    else:
-        pass
-
-    array = [0, 1, 2, 3, 4]
-
-    #flaw # check that tainted_3 < len(array), but not that it is >= 0, so may attempt to read out of array bounds
-    if tainted_3 < len(array):
-        print(array[tainted_3])
-    else:
-        print('Array index out of bounds')
-
-    print('Done')
+    #flaw
+    os.system('ls ' + tainted_1)
 
 
 if __name__ == '__main__':
