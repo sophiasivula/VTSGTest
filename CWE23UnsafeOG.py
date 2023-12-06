2bd9a8c127358e622571f0c2291c3cf23069ed4c
sophiasivula <sivu7048@vandals.uidaho.edu>
2023-12-05 18:01:07 -0800

    Update CWE23UnsafeOG.py

--- CWE23UnsafeOG.py
+++ CWE23UnsafeOG.py
@@ -4,22 +4,31 @@ import sys
 
 def main():
 
-    # no input
-    tainted_2 = None
+    try:
+        tainted_2 = int(sys.argv[1])
+    except ValueError:
+        tainted_2 = 1776
 
     tainted_3 = tainted_2
 
-    if 1==1:
+    if 1==0:
 
-        tainted_3 = input() # read one line
+        if tainted_2 < 0:
+            sys.exit("Negative input not allowed")
+        tainted_3 = tainted_2
 
+    else:
+        pass
 
-    if tainted_3 is not None:
-        #flaw # no validation - concatenated value allows arbitrary execution
-        sys.path += [tainted_3]
-        print(f'added { tainted_3 } to Python module search path')
+    array = [0, 1, 2, 3, 4]
 
-    print('Finished')
+    #flaw # check that tainted_3 < len(array), but not that it is >= 0, so may attempt to read out of array bounds
+    if tainted_3 < len(array):
+        print(array[tainted_3])
+    else:
+        print('Array index out of bounds')
+
+    print('Done')
 
 
 if __name__ == '__main__':
