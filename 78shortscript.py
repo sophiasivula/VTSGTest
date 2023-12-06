36ea47c4a4c9823c88a88528f892d0fa1b6f8977
sophiasivula <sivu7048@vandals.uidaho.edu>
2023-12-05 18:38:05 -0800

    Update 78shortscript.py

--- 78shortscript.py
+++ 78shortscript.py
@@ -131,7 +131,7 @@ def test():
         sys.exit(1)
 
     # Safe command execution using subprocess
-    subprocess.run(['ls', tainted_1], check=True)
+    subprocess.run(['usr/bin/ls', tainted_1], check=True)
 
 if __name__ == '__main__':
     test()
